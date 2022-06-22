76392be1e9446353e58db425090af633a2d77501
Svasiok <izuz2111@gmail.com>
2022-06-22 17:39:34 +0300

    Adding Unique Selectors

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+	
+class ProductPageLocators():
+    PRODUCT_NAME = (By.XPATH, '//div[2]/div/div[1]/div[1]/div/strong')
+    PRICE_PRODUCT = (By.XPATH, '//div[2]/div/div[1]/div[3]/div/p[1]/strong')
+    BASKET_FORM = (By.CSS_SELECTOR, "#add_to_basket_form")
+    PRODUCT_NAME_ADD = (By.XPATH, '//div[2]/div/div[2]/div[2]/article/div[1]/div[2]/h1')
+    PRICE_PRODUCT_ADD = (By.XPATH, '//div[2]/div/div[2]/div[2]/article/div[1]/div[2]/p[1]')
