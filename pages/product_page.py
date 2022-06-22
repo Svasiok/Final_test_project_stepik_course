c6ce39b9a9a9dda4e17ca31369d9ce5ff50ac5d2
Svasiok <izuz2111@gmail.com>
2022-06-22 17:37:28 +0300

    Making Data Independence

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,13 +10,15 @@ class ProductPage(BasePage):
         
         
     def should_be_same_product_name(self):
+         item_name_add = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_ADD)
          item_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
          print(item_name.text)
-         assert item_name.text == "The shellcoder's handbook", 'product name is not same'
+         assert item_name.text == item_name_add.text, 'product name is not same'
          assert True
 		 
     def should_be_same_price(self):
+         price_add = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT_ADD)
          price = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT)
          print(price.text)
-         assert price.text == "£9.99", 'product price is not same'
+         assert price.text == price_add.text, 'product price is not same'
          assert True
