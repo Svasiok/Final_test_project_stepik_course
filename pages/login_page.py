223ab422332d8aa4914b1852aa304133a58eb22f
Svasiok <izuz2111@gmail.com>
2022-06-24 15:23:47 +0300

    Added to LoginPage the register_new_user (email, password) method, which takes two lines and registers the user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -18,3 +19,13 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
         assert True
+		
+    def register_new_user(self, email, password):
+        input_email = self.browser.find_element(*LoginPageLocators.EMAIL)
+        input_password = self.browser.find_element(*LoginPageLocators.PASSWORD)
+        input_Confirm_password = self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD)
+        input_email.send_keys(email)
+        input_password.send_keys(password)
+        input_Confirm_password.send_keys(password)
+        register_button = self.browser.find_element(*LoginPageLocators.REG_BUTTON)
+        register_button.click()		
