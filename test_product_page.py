b567e4f615d29dd31d85d41c2c4efd379ef975ed
Svasiok <izuz2111@gmail.com>
2022-06-24 15:33:43 +0300

    Added a new class for TestUserAddToBasketFromProductPage tests.

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
 import time
 
@@ -31,10 +32,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.should_not_be_message_success()
 	
-def test_guest_cant_see_success_message(browser):
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_not_be_message_success()
+
 @pytest.mark.xfail	
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
@@ -60,3 +58,36 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket_page()
+	
+	
+@pytest.mark.login_user	
+class TestUserAddToBasketFromProductPage():
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        register_page = LoginPage(browser, link)
+        register_page.open()
+        email = str(time.time()) + "@fakemail.org"
+        password = email
+        register_page.register_new_user(email, password)
+        register_page.should_be_authorized_user()
+		
+        
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_message_success()
+	
+  
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_same_product_name()
+        page.should_be_same_price()
