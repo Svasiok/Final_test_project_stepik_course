aec6d8155007f3465af08d27ccb82bb59e3d4259
Svasiok <izuz2111@gmail.com>
2022-06-24 15:21:37 +0300

    add new LoginPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -7,6 +7,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REG_BUTTON = (By.XPATH, "//div[2]/div/div[2]/div[2]/div/div[2]/form/button")
+    
 	
 class ProductPageLocators():
     PRODUCT_NAME = (By.XPATH, '//div[2]/div/div[1]/div[1]/div/strong')
@@ -18,8 +23,9 @@ class ProductPageLocators():
 	
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link")
     BASKET_LINK = (By.XPATH, '//header/div[1]/div/div[2]/span')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 	
 class BasketPageLocators():
      BASKET_ITEM = (By.CSS_SELECTOR, "div.basket-items")
